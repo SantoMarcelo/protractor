da6526d6facf8b878a1e95bd2aa0d3cabaa34086
Marcelo Inphonex <marcelo.santos@inphonex.com>
2017-07-21 16:39:54 -0300

    firts test login

--- new-folder/spec.js
+++ new-folder/spec.js
@@ -2,19 +2,33 @@
  * Created by Marcelo Inphonex on 21/07/2017.
  */
 // spec.js
-describe('angularjs todo mvc homepage', function() {
-    it('should have a title', function() {
+describe('Login in R! Application', function() {
+
+      it('should be able to login with an admin user', function() {
+       //access R! page
         browser.get('http://login.qc.dev.ringbyname.com/current');
+        expect(browser.getTitle()).toEqual('RingByName');
+
+        //make login with admin user
+        element(by.name('username')).sendKeys('devqc.user4@ringbyname.com');
+        element(by.model('data.password')).sendKeys('123456asd');
+        element(by.buttonText('Sign In')).click();
 
         expect(browser.getTitle()).toEqual('RingByName');
-    });
-});
 
-describe('Login in R! application', function()){
-    it('should be able to login with a admin user', function() {
-
-        element(by.id('username')).sendKeys('devqc.user4@ringbyname.com');
-        element(by.name('password')).sendKeys('123456asd');
-        element(by.)
     })
-}
+    it('login with wrong user', function(){
+            //access R! page
+            browser.get('http://login.qc.dev.ringbyname.com/current');
+            expect(browser.getTitle()).toEqual('RingByName');
+
+            //make login with admin user
+            element(by.name('username')).sendKeys('devqc.userx@ringbyname.com');
+            element(by.model('data.password')).sendKeys('123456asd');
+            element(by.buttonText('Sign In')).click();
+
+            expect(browser.getTitle()).toEqual('RingByName');
+
+    }
+    )
+});
